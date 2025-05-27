ba38ff5313cf2bf3a798ea536116487ac6d0e92d
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-05-27 10:05:59 +0300

    Add project files.

---
+++ Commando/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commando
+{
+     public class Weapon
+    {
+        public string name;
+        public string manufacturer;
+        public int bulletsNumber;
+
+        public Weapon(string Name, string Manufacturer, int BulletsNumber)
+        {
+            this.name = Name;
+            this.manufacturer = Manufacturer;
+            this.bulletsNumber = BulletsNumber;
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Shooting");
+            this.bulletsNumber--;
+        }
+    }
+}
---
+++ Commando/Commando.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commando
+{
+    internal class Commando
+    {
+        public string name;
+        public string codeName;
+        public string[] tools = new string[] { "hammer", "scalpel", "rope", "bag", "canteen" };
+        public string status = "stand";
+
+
+        public Commando(string Name, string CodeName, string[] Tools, string Status)
+        {
+            this.name = Name;
+            this.codeName = CodeName;
+        }
+
+        public void Walk()
+        {
+            this.status = "walk";
+            Console.WriteLine($"the warrior is {this.status}");
+        }
+
+        public void Hide()
+        {
+            this.status = "hide";
+            Console.WriteLine($"the warrior is {this.status}");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine($"Commando with code name {this.codeName} attacking now");
+        }
+    }
+}
