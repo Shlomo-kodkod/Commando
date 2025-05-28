41eac888acf47d9280ae487a6c983e2f93433be2
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-05-28 09:42:39 +0300

    add EnemyFactory class

---
+++ Commando/EnemyFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commando
+{
+    public class EnemyFactory
+    {
+        private List<Enemy> enemies = new List<Enemy>();
+
+
+        public Enemy CreatEnemy(string name)
+        {
+            Enemy enmey = new Enemy(name);
+            return enmey;
+        }
+
+        public List<Enemy> GetAllEnemy()
+        {
+            return this.enemies;
+        }
+    }
+}
