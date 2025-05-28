dac8bf23376590ba25ebbe9ebf401c696ca7b2cf
Shlomo-Kodkod <quarts.91.elders@icloud.com>
2025-05-28 09:35:07 +0300

    add enemy class

---
+++ Commando/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commando
+{
+    public class Enemy
+    {
+        string Name;
+        int Life = 100;
+        
+        public Enemy(string name)
+        {
+            this.Name = name;
+        }
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        public bool IsAlive()
+        {
+            return this.Life > 0;
+        }
+
+        static void Scream()
+        {
+            Console.WriteLine("I'm an enemy");
+        }
+    }
+}
